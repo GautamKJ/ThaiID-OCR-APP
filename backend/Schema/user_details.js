9ca34bdc53443a6062908424ef6c079af5a6e7ce
Gautam <jha.gautam306@gmail.com>
2023-12-24 15:45:17 +0530

    Successfully added mongodb database and made the schema

---
+++ backend/Schema/user_details.js
@@ -0,0 +1,47 @@
+const mongoose= require('mongoose');
+const {Schema}=mongoose;
+const userSchema= new Schema({
+
+    identification_number:{
+        type:String,
+        required:true
+    },
+
+    first_name:{
+        type:String,
+        required:true
+    },
+    last_name:{
+        type:String,
+        required:true
+    },
+    dob:{
+        type:String,
+        required:true
+    },
+    issue_date:{
+        type:String,
+        required:true
+    },
+    expiry_date:{
+        type:String,
+        required:true
+    },
+    status:{
+       type:String,
+       required:true
+    },
+    date:{
+        type:Date,
+        default:new Date
+        
+    }
+
+})
+
+// Now, we need to export and create our Model. So call the module. exports and we want to export the mongoose model and we need to specify arguments to this model() method. The first argument is gonna be the name of the model. So let’s name our model as “Employee“, and the second argument is our Schema that is employeeSchema.
+
+const User=mongoose.model('user',userSchema);;
+User.createIndexes();
+module.exports=User;
+
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express=require('express');
+const database = require('./db');
+require("dotenv").config();
 const port= process.env.PORT || 8081;
 const app= express();
 
 
-app.use(express.json());
 
+app.use(express.json());
+database();
 
 app.listen(port, function () {
  
