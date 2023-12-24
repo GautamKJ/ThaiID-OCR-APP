d159f82702efdef4de53947f39daa4d4fedd3153
Gautam <jha.gautam306@gmail.com>
2023-12-25 04:23:39 +0530

    Added View Page where Records are display

--- frontend/ocr-app/src/Component/View.js
+++ frontend/ocr-app/src/Component/View.js
@@ -2,6 +2,69 @@ import React, { useContext, useEffect, useState } from 'react'
 import {Link,useNavigate} from "react-router-dom";
 
 export default function View() {
+	// state for record
+	const [detail,setDetail]=useState([]);
+
+	// set image
+	const[image,setImage]=useState();
+
+	// delete record
+
+	const deleteItem=async(id)=>{
+		// window.alert(id);
+		try {
+			
+			const response=await fetch(`http://localhost:8081/api/deleterecord/${id}`,{
+			  method:"POST",
+			  headers:{
+				// 'content-Type':'application/json',
+				// 'auth-token':localStorage.getItem('token')
+			  },
+			//   body:JSON.stringify({complain_status:currstatus})
+			  
+			});
+			// let json= await response.json();
+			fetchdata();
+			// console.log(json);
+		}
+		catch (error) {
+ 
+			console.log (error);
+		  }
+	}
+
+	const fetchdata= async()=>{
+		
+	
+		try {
+	   
+		  const response=await fetch("http://localhost:8081/api/fetchrecord",{
+			method:"GET",
+			headers:{
+			//   'content-Type':'application/json',
+			//   'auth-token':localStorage.getItem('token')
+			},
+			
+		  });
+		  let json= await response.json();
+		  setDetail(json);
+		  
+		  console.log(detail);
+		 
+		  
+		}
+		catch (error) {
+	   
+		  console.log (error);
+		}
+	}
+
+	useEffect(()=>{
+   
+		fetchdata();
+	   
+	  },[]);
+
   return (
     <div>
       <section id="content">
@@ -32,30 +95,41 @@ export default function View() {
 						
                            
 					</div>
+					{ detail.length>0 && 
+					<div class="tableContainer">
 					<table>
 						<thead>
 							<tr>
+								<th>S.No</th>
 								<th>User</th>
-								<th>Date</th>
-                                <th>Date of Birth</th>
+								<th>Issue Date</th>
+                                <th>Expiry Date</th>
 								<th>Status</th>
 							</tr>
 						</thead>
-						<tbody>
-							<tr>
+						<tbody >
+							{
+								detail.map((data,index)=>{
+									return (
+							<tr key={index}>
+								<td>{index+1}</td>
 								<td>
-									<img src="img/people.png"/>
-									<p>John Doe</p>
+									<img src="https://www.sinosecu.com.cn/upload/20211018/KXa2NPVvXF278Wr6gTR.jpg"/>
+									<p>{data.first_name + " "+data.lastName
+									}</p>
 								</td>
-                                <td>24 DEC</td>
-                                <td>24 AUG 2002</td>
+                                <td>{data.issueDate}</td>
+                                <td>{data.dob}</td>
 								
-								<td><span class="status completed">Completed</span></td>
-                                <td><i class='bx bxs-like bx-spin'></i></td>
+								<td><span class="status completed"  >{data.status}</span></td>
+                     <td ><button onClick={async() => await deleteItem(data._id)} ><i class='bx bxs-trash' ></i></button></td>
 							</tr>
-							
+									)
+								})}
 						</tbody>
 					</table>
+					</div>
+}
 				</div>
 				
 			</div>
