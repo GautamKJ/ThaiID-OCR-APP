ab0bd9507b2ecfcdc973f09a414565d12b10c5c4
Gautam <jha.gautam306@gmail.com>
2023-12-25 23:12:01 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const database = require('./db');
 require("dotenv").config();
+const path=require('path');
 var cors = require('cors');
 
 
@@ -10,13 +11,29 @@ var app= express();
 
 app.use(cors());
 
-app.use(express.json());
 
+
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send("Server is working");
+})
 database();
 
 app.use('/api/', require('./routes/createUser'));
 app.use('/api/', require('./routes/fetchRecord'));
 app.use('/api/', require('./routes/deleteRecord'));
+
+app.use(express.static(path.join(__dirname,  'build')));
+console.log(path.join(__dirname, '../frontend/ocr-app', 'build'));
+console.log("asfasdf");
+
+
+// app.get('*', (req, res) => {
+
+//     const _path =path.resolve(__dirname,  'build', 'index.html');
+//     console.log("afd ",_path);
+//     res.sendFile(_path);
+// })
 
 app.listen(PORT, function () {
  
